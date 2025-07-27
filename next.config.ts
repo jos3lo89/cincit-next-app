9ef92b5b1960450be7678990d3366bba746ac84c
jos3lo89 <ganbaru743@gmail.com>
2025-07-27 06:18:26 -0500

    Fix: sever action body size

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   // allowedDevOrigins: ["local-origin.dev", "*.local-origin.dev"],
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "6mb",
+    },
+  },
 };
 
 export default nextConfig;
