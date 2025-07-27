5879b6e2dfe8e0c0858d600a2e221af0ddea3187
jos3lo89 <ganbaru743@gmail.com>
2025-07-27 06:06:45 -0500

    Refactor: institution-list api

--- app/api/institution-list/route.ts
+++ app/api/institution-list/route.ts
@@ -3,21 +3,12 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   const institutionList = await prisma.user.findMany({
+    distinct: ["institution"],
     select: {
       id: true,
       institution: true,
     },
   });
 
-  const uniqueMap = new Map<string, InstitutionUser>();
-  for (const user of institutionList) {
-    if (user.institution && !uniqueMap.has(user.institution)) {
-      uniqueMap.set(user.institution, {
-        id: user.id,
-        institution: user.institution,
-      });
-    }
-  }
-  const result = Array.from(uniqueMap.values());
-  return NextResponse.json(result);
+  return NextResponse.json(institutionList);
 }
