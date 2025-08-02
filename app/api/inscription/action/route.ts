ec0f179e57092b5eb8294356822c8654312c0e40
jos3lo89 <ganbaru743@gmail.com>
2025-08-01 21:04:38 -0500

    Feat: api inscription action

--- app/api/inscription/action/route.ts
+++ app/api/inscription/action/route.ts
@@ -1,12 +1,47 @@
+import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
+import z from "zod";
+
+const querySchema = z.object({
+  id: z
+    .string()
+    .transform(Number)
+    .refine((val) => !isNaN(val), {
+      message: "ID inválido",
+    }),
+  state: z.enum(["approved", "rejected"], { message: "Estado inválido" }),
+});
 
 export const GET = async (req: NextRequest) => {
   try {
     const searchParams = req.nextUrl.searchParams;
     const idInscription = searchParams.get("id");
-    const state = searchParams.get("state");
+    const stateInscription = searchParams.get("state");
 
-    return NextResponse.json({ idInscription, state });
+    const result = querySchema.safeParse({
+      id: idInscription,
+      state: stateInscription,
+    });
+
+    if (!result.success) {
+      return NextResponse.json(
+        { message: "Error parametros erroneos" },
+        { status: 400 }
+      );
+    }
+
+    const { id, state } = result.data;
+
+    const updateInscription = await prisma.inscription.update({
+      where: {
+        id,
+      },
+      data: {
+        state,
+      },
+    });
+
+    return NextResponse.json(updateInscription, { status: 200 });
   } catch (error) {
     console.log("error: /api/inscription/action:", error);
     return NextResponse.json(
