e999ed0458c3a8cf0bbbc8c4a28467435bed18ba
jos3lo89 <ganbaru743@gmail.com>
2025-08-01 20:46:30 -0500

    add example action route

--- app/api/inscription/[id]/approved/route.ts
+++ app/api/inscription/[id]/approved/route.ts
@@ -1,12 +1,17 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export const PUT = async (
-  req: Request,
+  req: NextRequest,
   { params }: { params: Promise<{ id: string }> }
 ) => {
   try {
     const { id } = await params;
     console.log("id: ", id);
+
+    // query
+    const searchParams = req.nextUrl.searchParams;
+    const idInscription = searchParams.get("id");
+    const state = searchParams.get("state");
 
     return NextResponse.json({ message: "id: " + id });
   } catch (error) {
---
+++ app/api/inscription/action/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (req: NextRequest) => {
+  try {
+    const searchParams = req.nextUrl.searchParams;
+    const idInscription = searchParams.get("id");
+    const state = searchParams.get("state");
+
+    return NextResponse.json({ idInscription, state });
+  } catch (error) {
+    console.log("error: /api/inscription/action:", error);
+    return NextResponse.json(
+      { message: "Error interno del  servidor." },
+      { status: 500 }
+    );
+  }
+};
