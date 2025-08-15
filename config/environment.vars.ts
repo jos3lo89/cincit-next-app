2de87933af9f78afb5b707915f4de288ce1dc8c0
jos3lo89 <ganbaru743@gmail.com>
2025-08-15 10:22:34 -0500

    fix: enviroments not found

--- config/environment.vars.ts
+++ config/environment.vars.ts
@@ -9,6 +9,7 @@ const envVar = object({
   EMAIL_PORT: string(),
   JWT_SECRET: string(),
   AUTH_SECRET: string(),
+  AUTH_URL: string(),
   NEXT_PUBLIC_URL_IMG_SERVICE: string(),
   MY_URL_SITE: string(),
 });
